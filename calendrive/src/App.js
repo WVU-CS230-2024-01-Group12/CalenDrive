a3caad9ef7d57a686aa347670691af380afe9613
sjh-00028 <143116678+sjh-00028@users.noreply.github.com>
2024-04-01 02:08:26 -0400

    Create first half of OAuth2 workflow (#19)
    * Finish first half of OAuth2 workflow
    
    * Fix a comment
    
    * Fix further merge issues
    
    * Put back the `/authenticate_url` handler

--- calendrive/src/App.js
+++ calendrive/src/App.js
@@ -1,13 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import React from 'react';
-import InteractiveCalendar from './InteractiveCalendar.js';
-
+import MainPage from './Pages/MainPage';
+import OAuth2Callback from './Pages/OAuth2Callback';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-center mt-3">CalendDrive</h1>
-      <InteractiveCalendar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/oauth2callback" element={<OAuth2Callback />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
