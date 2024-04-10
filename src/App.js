373c660b154a2497f18b0671897f4555c50c8329
Sephagus <sethmcbee@outlook.com>
2024-04-10 15:22:12 -0400

    Added functionality to pass in lon/lat

--- src/App.js
+++ src/App.js
@@ -8,12 +8,7 @@ function App() {
     <div className="App">
       <h1 className="text-center mt-3">Interactive Calendar</h1>
       <InteractiveCalendar />
-      <Map
-        height="300px"
-        width="400px"
-        longitude={39.631111}
-        latitude={-79.957199}
-      />
+      <Map height="300px" width="400px" lon={-79.953} lat={39.634} />
     </div>
   );
 }
--- src/Map.js
+++ src/Map.js
@@ -1,8 +1,10 @@
 import { Marker, MapContainer, TileLayer, useMap } from "react-leaflet";
 import React from "react";
 import "leaflet/dist/leaflet.css";
+import markerIconPng from "leaflet/dist/images/marker-icon.png";
+import { Icon } from "leaflet";
 
-function Map({ height, width, longitude, latitude }) {
+function Map({ height, width, lat, lon }) {
   function ChangeView({ center, zoom }) {
     const map = useMap();
     map.setView(center, zoom);
@@ -10,17 +12,22 @@ function Map({ height, width, longitude, latitude }) {
   }
 
   return (
-    <MapContainer
-      center={{ longitude, latitude }}
-      zoom={13}
-      style={{ height, width }}
-    >
-      <ChangeView center={{ longitude, latitude }} zoom={13} />
+    <MapContainer center={[lat, lon]} zoom={16} style={{ height, width }}>
+      <ChangeView center={[lat, lon]} zoom={16} />
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
       />
-      <Marker position={{ longitude, latitude }}></Marker>
+      <Marker
+        position={[lat, lon]}
+        icon={
+          new Icon({
+            iconUrl: markerIconPng,
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+          })
+        }
+      ></Marker>
     </MapContainer>
   );
 }
