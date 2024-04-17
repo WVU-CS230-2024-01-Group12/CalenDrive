e60a7397e104fe2a99f6dc18033114aff0814012
Skish03 <143004103+Skish03@users.noreply.github.com>
2024-04-17 13:51:54 -0400

    navbar theme

--- calendrive/src/Navbar/Nav.js
+++ calendrive/src/Navbar/Nav.js
@@ -1,15 +1,37 @@
 import "./Nav.css"
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import StyledLink from "./StyledLink";
 
 function Nav() {
+    const [backgroundColor, setBackgroundColor] = useState(""); 
+
+    const handleColorChange = (event) => {
+        const selectedColor = event.target.value;
+        setBackgroundColor(selectedColor);
+    };
 
     return(
 
-        <div className="navbar">
+        <div className="navbar" style={{ backgroundColor: backgroundColor }}>
             <StyledLink to="/"><div className="navigate"><img src="calendriveLogo.png" /></div></StyledLink>
             <StyledLink to="/edit-event"><div className="navigate">Edit Events</div></StyledLink>
             <StyledLink to="/"><div className="navigate">Profile</div></StyledLink>
+        
+        <div className="dropdown">Change Theme:
+            <button className="dropbtn"> 
+                <i className="fa fa-caret-down"></i>
+            <select id="themeSelector" onChange={handleColorChange}>
+                <option value="#808080">Gray</option>
+                <option value="#ff0000">Red</option>
+                <option value="#008000">Green</option>
+                <option value="#0000ff">Blue</option>
+                <option value="#8A2BE2">Purple</option>
+                <option value="#FF69B4">Pink</option>
+            </select>
+        </button>
+    </div>
+
         </div>
     );
 }
