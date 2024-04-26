37ce9fd67774186257b6f81bf33f4c258defa231
sjh-00028 <sjh00028@mix.wvu.edu>
2024-04-26 01:25:40 -0400

    Add basic profile page And cache account info in sessionStorage

--- calendrive/src/Navbar/Nav.js
+++ calendrive/src/Navbar/Nav.js
@@ -15,7 +15,7 @@ function Nav() {
         <div className="navbar" style={{ backgroundColor: backgroundColor }}>
             <StyledLink to="/"><div className="navigate"><img src="calendriveLogo.png" alt="Calendrive logo"/></div></StyledLink>
             <StyledLink to="/edit-event"><div className="navigate">Edit Events</div></StyledLink>
-            <StyledLink to="/"><div className="navigate">Profile</div></StyledLink>
+            <StyledLink to="/profile"><div className="navigate">Profile</div></StyledLink>
             <UserLogin />
         
             <div className="dropdown">Change Theme:
