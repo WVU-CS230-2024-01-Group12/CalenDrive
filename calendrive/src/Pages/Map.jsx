1ffa06f8d821c3f057625ce43b481e9497474f39
Sephagus <sethmcbee@outlook.com>
2024-04-26 16:02:54 -0400

    Added map functionality

---
+++ calendrive/src/Pages/Map.jsx
@@ -0,0 +1,42 @@
+import { Marker, MapContainer, TileLayer, useMap } from "react-leaflet";
+import React from "react";
+import "leaflet/dist/leaflet.css";
+import markerIconPng from "leaflet/dist/images/marker-icon.png";
+import { Icon } from "leaflet";
+
+function Map({ height, width, lat, lon }) {
+  function ChangeView({ center, zoom }) {
+    const map = useMap();
+    map.setView(center, zoom);
+    return null;
+  }
+
+  if (isNaN(lat) || isNaN(lon)) {
+    lat = 0;
+    lon = 0;
+  }
+
+  return (
+    <div>
+      <MapContainer center={[lat, lon]} zoom={16} style={{ height, width }}>
+        <ChangeView center={[lat, lon]} zoom={16} />
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+        />
+        <Marker
+          position={[lat, lon]}
+          icon={
+            new Icon({
+              iconUrl: markerIconPng,
+              iconSize: [25, 41],
+              iconAnchor: [12, 41],
+            })
+          }
+        ></Marker>
+      </MapContainer>
+    </div>
+  );
+}
+
+export default Map;
